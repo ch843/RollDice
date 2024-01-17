a170403d5ddaa9cd207861af05cf94fa4bd9e450
Cassidy Hardisty <Cassidy Hardisty@DESKTOP-IUJL50H>
2024-01-17 10:27:58 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string numRolls = "";
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        numRolls = System.Console.ReadLine("How many dice rolls would you like to simulate?");
+
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1 % of the total number of rolls.");
+        System.Console.WriteLine(" Total number of rolls = 1000.");
+
+    }
+}
